41b63ec39d0dee497ddefab10e26bcd2b51cba58
Manuel Mannhardt <manuel.mannhardt95@gmail.com>
2023-02-15 22:25:54 +0100

    refactored the operator precedence

--- Compiler/SyntaxFacts.py
+++ Compiler/SyntaxFacts.py
@@ -2,11 +2,13 @@ from Compiler.SyntaxKind import SyntaxKind
 
 
 class SyntaxFacts:
+    operators = {
+        SyntaxKind.PLUS_TOKEN: 1,
+        SyntaxKind.MINUS_TOKEN: 1,
+        SyntaxKind.STAR_TOKEN: 2,
+        SyntaxKind.SLASH_TOKEN: 2,
+    }
+
     @staticmethod
     def get_binary_operator_precedence(kind: SyntaxKind) -> int:
-        if kind in [SyntaxKind.PLUS_TOKEN, SyntaxKind.MINUS_TOKEN]:
-            return 1
-        elif kind in [SyntaxKind.STAR_TOKEN, SyntaxKind.SLASH_TOKEN]:
-            return 2
-
-        return 0
+        return SyntaxFacts.operators.get(kind, 0)
